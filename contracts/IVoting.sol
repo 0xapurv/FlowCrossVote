0852c237f63fbd446fccd1c66300e961fff91b98
Madhav <108409701+Madhav-Gupta-28@users.noreply.github.com>
2023-06-26 15:32:02 +0530

    Update IVoting.sol

--- contracts/IVoting.sol
+++ contracts/IVoting.sol
@@ -1,210 +1,131 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@routerprotocol/evm-gateway-contracts@1.1.11/contracts/IDapp.sol";
+import "@routerprotocol/evm-gateway-contracts@1.1.11/contracts/IGateway.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-import "@routerprotocol/router-crosstalk/contracts/RouterCrossTalk.sol";
-
-contract Voting is RouterCrossTalk {
-
-     uint256 private _crossChainGasLimit;
-    uint256 private _crossChainGasPrice;
-
+contract Voting is IDapp, Ownable {
     struct Candidate {
         string name;
         uint256 voteCount;
     }
 
     Candidate[] public candidates;
-    address public owner;
     mapping(address => bool) public voters;
 
     uint256 public votingStart;
     uint256 public votingEnd;
 
-
-    error CustomError(string message);
+    IGateway public gatewayContract;
 
     constructor(
         string[] memory _candidateNames,
         uint256 _durationInMinutes,
-        address _genericHandler
-    ) RouterCrossTalk(_genericHandler) {
+        address payable gatewayAddress
+    ) {
         for (uint256 i = 0; i < _candidateNames.length; i++) {
-            candidates.push(
-                Candidate({ name: _candidateNames[i], voteCount: 0 })
-            );
+            candidates.push(Candidate({
+                name: _candidateNames[i],
+                voteCount: 0
+            }));
         }
-        owner = msg.sender;
         votingStart = block.timestamp;
         votingEnd = block.timestamp + (_durationInMinutes * 1 minutes);
-        // RouterCrossTalk(_genericHandler);
-       
-    }
-
-    function _routerSyncHandler(bytes4 _selector, bytes memory _data)
-        internal
-        virtual
-        override
-        returns (bool, bytes memory)
-    {   
-        if (_selector == bytes4(keccak256("getAllVotesOfCandidates(uint8)"))) {
-        (uint8 targetChainId) = abi.decode(_data, (uint8));
-        Candidate[] memory fetchedCandidates = getAllVotesOfCandidates();
-          bytes memory returnData = abi.encode(fetchedCandidates);
-        emit FetchedCandidates(targetChainId, fetchedCandidates);
-        return (true, returnData);
-    }
-
-        (address _to, uint256 _amt) = abi.decode(_data, (address, uint256));
-        (bool success, bytes memory returnData) = address(this).call(
-            abi.encodeWithSelector(_selector, _to, _amt)
-        );
-        return (success, returnData);
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action.");
-        _;
-    }
-
-    // admin functions 
-     function _approveFees(address _feeToken, uint256 _amount)
-        external
-        onlyOwner
-    {
-        approveFees(_feeToken, _amount);
-    }
-
-       function setLinker(address _addr) external  onlyOwner {
-        setLink(_addr);
-    }
-
-    function setFeesToken(address _feeToken)
-        external
-        onlyOwner
-    {
-        setFeeToken(_feeToken);
+        gatewayContract = IGateway(gatewayAddress);
     }
 
 
-     function fetchCrossChainGasLimit() external view returns (uint256) {
-        return _crossChainGasLimit;
-    }
-
-
-     function replayTransferCrossChain(
-        bytes32 hash,
-        uint256 crossChainGasLimit,
-        uint256 crossChainGasPrice
-    ) public  {
-        routerReplay(hash, crossChainGasLimit, crossChainGasPrice);
-    }
-
-
-    // VOTING LOGIC STARTS HERE 
-
-
-    // ADD CANDIDATES FUNTIONALITY ------------------------
     function addCandidate(string memory _name) public onlyOwner {
-        candidates.push(Candidate({ name: _name, voteCount: 0 }));
+        candidates.push(Candidate({
+            name: _name,
+            voteCount: 0
+        }));
     }
 
+    function vote(uint256 _candidateIndex, bytes memory voterAddress) public {
+        address voter = toAddress(voterAddress);
+        require(!voters[voter], "You have already voted.");
+        require(_candidateIndex < candidates.length, "Invalid candidate index.");
 
-    function addCandidateCrossChain(uint8 _chainID, string memory _name) public onlyOwner returns(bool) {
-        bytes memory data = abi.encode("addCandidate(string)", _name);
-      
-        bytes4 _selector = bytes4(keccak256("addCandidate(string)"));
-        (bool success, ) = routerSend(
-        _chainID,
-        _selector,
-        data,
-        _crossChainGasLimit,
-        _crossChainGasPrice
-    );
-    require(success == true, "unsuccessful");
-    return success;
-    }
-// --------------------------------
-
-
-
-    // VOTE FUNCTION FUNCTIONALITY -----------------------------
-
-    function vote(address _voter, uint256 _candidateIndex) public {
-    require(!voters[_voter], "You have already voted.");
-    require(_candidateIndex < candidates.length, "Invalid candidate index.");
-
-    // Check if the function is called by the contract itself or by the actual voter
-    if (msg.sender != address(this)) {
-        require(msg.sender == _voter, "You can only vote for yourself.");
+        candidates[_candidateIndex].voteCount++;
+        voters[voter] = true;
     }
 
-    candidates[_candidateIndex].voteCount++;
-    voters[_voter] = true;
-}
-
-    // sending the cross chain voting request
-    function voteCrossChain(
-    uint8 _chainID,
-    uint256 _candidateIndex
-) external returns (bool) {
-    bytes memory data = abi.encode(msg.sender, _candidateIndex);
-    bytes4 _selector = bytes4(keccak256("vote(address,uint256)"));
-    (bool success, ) = routerSend(
-        _chainID,
-        _selector,
-        data,
-        _crossChainGasLimit,
-        _crossChainGasPrice
-    );
-    require(success == true, "unsuccessful");
-    return success;
-}
-
-// ------------------------------------------------------------
-
-
-// GETTING VOTES FROM DIFFERENT CHAINS -------------------------
-    event FetchedCandidates(uint8 chainID, Candidate[] candidates);
-
-    function getAllVotesOfCandidates() public view returns (Candidate[] memory) {
+    function getAllVotesOfCandiates() public view returns (Candidate[] memory) {
         return candidates;
     }
 
-    function getAllVotesOfCandidatesCrossChain(uint8 targetChainId) public {
-         bytes memory data = abi.encodeWithSignature("getAllVotesOfCandidates(uint8)", targetChainId);
-    // Execute the cross-chain request using Router CrossTalk
-    bytes4 _selector = bytes4(keccak256("getAllVotesOfCandidates(uint8)"));
-    (bool success,  ) = routerSend(
-        targetChainId,
-        _selector,
-        data,
-        _crossChainGasLimit,
-        _crossChainGasPrice
-    );
-
-    
-    require(success, "Cross-chain request failed");
-}
-       
-
-
-    // -------------------------------------------
-
     function getVotingStatus() public view returns (bool) {
-        return
-            (block.timestamp >= votingStart && block.timestamp < votingEnd);
+        return (block.timestamp >= votingStart && block.timestamp < votingEnd);
     }
 
     function getRemainingTime() public view returns (uint256) {
-        require(
-            block.timestamp >= votingStart,
-            "Voting has not started yet."
-        );
+        require(block.timestamp >= votingStart, "Voting has not started yet.");
         if (block.timestamp >= votingEnd) {
             return 0;
         }
+        return votingEnd - block.timestamp;
+    }
+
+    function setGateway(address gateway) external onlyOwner {
+        gatewayContract = IGateway(gateway);
+    }
+
+    function toAddress(bytes memory _address) internal pure returns (address) {
+        assembly {
+            mstore(add(0x20, _address), 20)
+        }
+        return address(uint160(uint256(keccak256(_address))));
+    }
+
+    function voteCrossChain(
+        uint256 _candidateIndex,
+        bytes calldata voterAddress,
+        string calldata destChainId,
+        bytes calldata requestMetadata
+    ) public {
+        require(getVotingStatus(), "Voting is not active.");
+
+        // Vote on behalf of the voter from the other chain
+        vote(_candidateIndex, voterAddress);
+
+        // Send the vote to the destination chain
+        bytes memory packet = abi.encode(_candidateIndex, voterAddress);
+        bytes memory requestPacket = abi.encode(packet);
+
+        gatewayContract.iSend(
+            1,
+            0,
+            string(""),
+            destChainId,
+            requestMetadata,
+            requestPacket
+        );
+    }
+
+    function iReceive(
+        string memory,
+        bytes memory packet,
+        string memory srcChainId
+    ) external override returns (bytes memory) {
+        require(msg.sender == address(gatewayContract), "only gateway");
+
+        (uint256 _candidateIndex, bytes memory voterAddress) = abi.decode(packet, (uint256, bytes));
+
+        // Vote on behalf of the voter from the source chain
+        vote(_candidateIndex, voterAddress);
+
+        return abi.encode(srcChainId);
+    }
+
+    
+    function iAck(uint256 requestIdentifier, bool execFlags, bytes memory execData) external override {
+        // Implementation of the iAck function, which is required by the IDapp interface
+        // Add your code here
+    }
+}
+
         return votingEnd - block.timestamp;
     }
 
