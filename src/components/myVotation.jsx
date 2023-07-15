20b58b1a9dd655da54a9bb6ba157f1a1db394522
Jazthedev <joelpepa2@gmail.com>
2023-07-15 15:01:47 +0200

    add functionality to my votation

--- src/components/myVotation.jsx
+++ src/components/myVotation.jsx
@@ -1,10 +1,36 @@
 import Link from "next/link";
 import { useState } from "react";
+import useVotingApp from "./useFlowVottingApp";
 export default function MyVotation({name,uuid}){
     const [address,setAddress] = useState('')
     const [errorMessage,setErrorMessage] = useState('')
-    function onaddCandidate(){
+    const [succefulMessage,setSuccefulMessage] = useState('')
+    const votingapp = new useVotingApp()
+    async function onaddCandidate(){
+        if(address>"0xc6a01f56e1ff8764"){
+            setErrorMessage("please enter a valid address")
+            return 
+        }
+        if(address.substring(0,2)!="0x"){
+            setErrorMessage("please enter a valid address")
+            return  
+        }
+        setErrorMessage("")
+        const scriptdata = await votingapp.scriptIsUserSignedUp(address)
+        if(scriptdata.data == false){
+            setErrorMessage("the candidate has to login the app before to become a possible candidate")
+            return  
+        }
+        const data = await votingapp.transactionAddCandidate(uuid,address)
+        console.log(data)
+        setSuccefulMessage("candidate added succefully")
+    }
 
+    function onChangeAddress (e){
+        if(e.target.value.length>"0xc6a01f56e1ff8764"){
+            return 
+        }
+        setAddress(e.target.value)
     }
     return <div>
         <br/>
@@ -15,9 +41,10 @@ export default function MyVotation({name,uuid}){
         
         <div>
             <div>add candidate address</div>
-            <input/>
-            <div>{errorMessage}</div>
-            <button>Add Candidate</button>
+            {errorMessage.length > 0 ? <div>error message:{errorMessage}</div> : <div></div>}
+            {succefulMessage.length >0 ? <div>succefull message:{succefulMessage}</div> : <div></div>}
+            <input value={address} placeholder="0xc6a01f56e1ff8764" onChange={onChangeAddress}className="text-black outline-secondary py-2 pl-4 w-3/5"/>
+            <button onClick={onaddCandidate}>Add Candidate</button>
         </div>
         <Link href={`${window.location.href}/votation/${uuid}`}>
             <div>
